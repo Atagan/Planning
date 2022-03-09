036ac0a0ddf627f62da47a5e287cbaf68977475d
Atagan <Laura.RamosMartinez@edu.uah.es>
2022-03-09 11:00:15 +0100

    Cosas del script

--- SegundaMatricula/PL1/Parte1/run-planners.py
+++ SegundaMatricula/PL1/Parte1/run-planners.py
@@ -13,29 +13,15 @@ def main():
         aux="drone_problem_d1_r0_l"+str((int(cantidad//2)))+"_p"+str((int(cantidad)))+"_c"+str((int(cantidad)))+"_g"+str((int(cantidad)))+"_ct2.pddl"
         problemas.append(aux)
 
-    
-
-    tiempo=0
-    contador=0
-    
     #Aqui ejecutamos los problemas sobre planificador FF, la ejecucion se detiene tras planear el ultimo problema o tras que un plan tarde más de 5 minutos en ejecutarse
-    tiemposFF=[]
-    while(tiempo<300000 and contador<len(problemas)):
-        inicio=current_milli_time()
-        cmd="./ff -o AidRelief.pddl -f " + problemas[contador]
-        os.system(cmd)
-        fin=current_milli_time()
-        tiempo=fin-inicio
-        #print("\n"+str(tiempo)+"\n")
-        contador+=1
-        tiemposFF.append(tiempo)
-
-    print(tiemposFF)
+    tiemposFF=ejecutar_planificador(problemas,"./ff -o AidRelief.pddl -f ")
 
     #TODO: ejecutar LPG-TD
     
     #TODO: ejecutar SGPLAN40
+
     #TODO: ejecutar SATPLAN
+
     #TODO: ejecutar FastDownward
 
 
@@ -44,7 +30,35 @@ def main():
         cmd="rm "+x
         os.system(cmd)
 
+
+def ejecutar_planificador(problemas, planificador):
+    tiempos=[]
+    tiempo=0
+    contador=0
+    while(tiempo<300000 and contador<len(problemas)):
+        print("Buscamos plan para el problema: "+str(contador)+"\n")
+        cmd=planificador + problemas[contador]
+        inicio=current_milli_time()
         
+        os.system(cmd)
+        fin=current_milli_time()
+        tiempo=fin-inicio
+        #print("\n"+str(tiempo)+"\n")
+        contador+=1
+        tiempos.append(tiempo)
+
+    if((contador)<len(problemas)):
+        print("Se ha excedido el tiempo máximo de 5 minutos, se ha terminado la ejecución en el problema: "+str(contador)+"\n")
+    else:
+        print("Se han completado todos los problemas.")
+
+    #Esto es opcional, el objetivo debería ser guardarlo en un fichero. TODO
+    print("Los tiempos (en segundos) necesarios para la ejecución han sido: ")
+    for i in tiempos:
+        print(str(i/1000)+"\n")
+
+    return tiempos
+
 def current_milli_time():
     return round(time.time() * 1000)
 
