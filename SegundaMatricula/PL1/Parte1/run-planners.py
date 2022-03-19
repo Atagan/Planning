9a3067ce10ccae3cd59c286a64149532870adca9
dferreiror <34660343+dferreiror@users.noreply.github.com>
2022-03-19 11:47:57 +0100

    última versión de código. CSV creado bien
    Se controló la posibilidad de que fuera 0 el salto y si el salto era 1 empezara con i=2

--- SegundaMatricula/PL1/Parte1/run-planners.py
+++ SegundaMatricula/PL1/Parte1/run-planners.py
@@ -2,34 +2,30 @@ from optparse import OptionParser
 import os
 import time
 import sys
+import csv
 
 def main():
-    resultados = []
+    resultados = {}
     problemas = crear_problemas()
 
     #ejecutar FF
-    resultados.append("FF: " + str(ejecutar_planificador_problema("ff","./ff -o AidRelief.pddl -f ", problemas)))
+    resultados['FF'] = ejecutar_planificador_problema("ff","./ff -o AidRelief.pddl -f ", problemas)
 
     #ejecutar LPG-TD
-    resultados.append("LPG-TD: " + str(ejecutar_planificador_problema("lpg-td","./lpg-td -o AidRelief.pddl -n 1 -f ", problemas)))
+    resultados['LPG-TD'] = ejecutar_planificador_problema("lpg-td","./lpg-td -o AidRelief.pddl -n 1 -f ", problemas)
 
     #ejecutar SGPLAN40
-    resultados.append("SGPLAN40: " + str(ejecutar_planificador_problema('sgplan40',"./sgplan40 -o AidRelief.pddl -out borrar.soln -f ",problemas)))
+    resultados['SGPLAN40'] = ejecutar_planificador_problema('sgplan40',"./sgplan40 -o AidRelief.pddl -out borrar.soln -f ",problemas)
 
     #ejecutar SATPLAN
-    resultados.append("SATPLAN: " + str(ejecutar_planificador_problema('satplan',"./satplan/satplan -solver siege -domain AidRelief.pddl -problem ", problemas)))
+    resultados['SATPLAN'] = ejecutar_planificador_problema('satplan',"./satplan/satplan -solver siege -domain AidRelief.pddl -problem ", problemas)
 
     #ejecutar FastDownward
-    resultados.append("FASTDOWNWARD: " + str(ejecutar_planificador_problema('FastDownward', "./singularity-ce-3.9.5/downward.sif --alias lama-first AidRelief.pddl ", problemas)))
+    resultados['FASTDOWNWARD'] = ejecutar_planificador_problema('FastDownward', "./singularity-ce-3.9.5/downward.sif --alias lama-first AidRelief.pddl ", problemas)
 
     grabar_resultados(resultados)
     limpiar_directorio(problemas)
 
-def grabar_resultados(resultados):
-    file = open('./resumen.txt', 'w+')
-    for resultado in resultados:
-        file.write(resultado + "\n")
-    file.close()
 
 def crear_problemas():
     parser = OptionParser(usage='python3 run-planners.py [-help] options...')
@@ -37,13 +33,15 @@ def crear_problemas():
     (options, args) = parser.parse_args()
 
     if options.salto is None:
-        print("Salto de 10 en 10")
-        salto = 10
+        print("Salto de 10 en 10"); salto = 10
+    elif options.salto is 0:
+    	salto = 1
     else:
         salto = options.salto
 
-    print("Creando problemas.\n")
-    i = 1; num = 0; problemas = []
+    print("Creando problemas ....\n")
+    num = 0; problemas = []
+    i = 2 if (salto == 1) else 1
     while (num < 100):
         num = salto * i
         os.system("python3 generate-problem.py -d 1 -r 0" + " -l " + str((int(num // 2))) + " -p " + str((int(num))) + " -c " + str((int(num))) + " -g " + str((int(num))))
@@ -51,16 +49,12 @@ def crear_problemas():
         i += 1
     return problemas
 
-def limpiar_directorio(problemas):
-    for x in problemas:
-        os.system("rm "+x)
-        os.system("rm plan_"+ x +"_1.SOL")
-    os.system("rm borrar.soln")
 
 def ejecutar_planificador_problema(planificador, comando, problemas):
     tiempos={}; contador=0; tiempo = 0; tiempoMax = 300000
     
-    contadorLimite = 10 if (planificador == 'satplan') else len(problemas)
+    #contadorLimite = 10 if (planificador == 'satplan') else len(problemas)
+    contadorLimite = len(problemas)
 
     while(tiempo<tiempoMax and contador<contadorLimite):
         print("Plan para el problema: "+str(contador)+"\n")
@@ -77,6 +71,24 @@ def ejecutar_planificador_problema(planificador, comando, problemas):
     else:
         print("Se han completado todos los problemas previstos.")
     return tiempos
+
+
+def grabar_resultados(resultados):
+    csv = open('./resumen.csv', 'w+')
+    for key in resultados:
+        csv.write("\n" + str(key) + "\n")
+        for key2 in resultados[key]:
+        	csv.write(str(key2) + ";" + str(resultados[key][key2]) + "\n")	
+    csv.close()
+ 
+    
+def limpiar_directorio(problemas):
+    for x in problemas:
+        os.system("rm "+x)
+        os.system("rm plan_"+ x +"_1.SOL")
+    os.system("rm borrar.soln")
+    os.system("rm CNF*")
+    os.system("rm sas_plan")
 
 if __name__ == '__main__':
     main()
